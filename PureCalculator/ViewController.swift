fa2fc803dbf2961c5998d26c8d90ecd91f69de44
Andrey Bogdanov <andbog@live.ru>
2020-07-21 18:15:23 +0300

    Initial Commit

---
+++ PureCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PureCalculator
+//
+//  Created by Andrey Bogdanov on 21.07.2020.
+//  Copyright © 2020 Andbog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
